f6bee5e1b6632da4782953b7508fdae72d1df81f
Wade Withers <97404242+withersw@users.noreply.github.com>
2023-02-04 20:42:54 -0700

    Update Main.java

--- High_Low/highLow/src/Main.java
+++ High_Low/highLow/src/Main.java
@@ -1,3 +1,5 @@
+// Create instances of Card and Director
+// Generate new card, display it and start the game.
 public class Main {
     public static void main(String[] args) {
         Card card = new Card();
