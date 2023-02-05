3ef9a0d937cac3fc35ce360cfa2394e1f3295271
Wade Withers <97404242+withersw@users.noreply.github.com>
2023-02-04 20:44:12 -0700

    Update Card.java

--- High_Low/highLow/src/Card.java
+++ High_Low/highLow/src/Card.java
@@ -1,13 +1,16 @@
+// Import Java library for arrays
 import java.util.ArrayList;
 
 public class Card {
+    // Create new array and card variable. 
     ArrayList<Integer> cards = new ArrayList<Integer>();
     public int card;
 
     public Card() {
 
     }
-    // Generate a random integer from 0 to 14 and add it to an array of integers.
+    // Generate a random integer from 0 to 14. 
+    // Add it to an array of integers.
     public int cardChoice() {
         card = (int)(Math.random() * 14);
         cards.add(card);
